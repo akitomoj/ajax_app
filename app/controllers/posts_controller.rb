41e9a74be3944b4ae9f079ebc792c80d4ebbd71e
akitomoj <akitomoj@gmail.com>
2020-11-21 13:10:29 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
  
   def index
-    @posts =  Post.all.order(id: "DESC")
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
@@ -19,4 +19,5 @@ class PostsController < ApplicationController
 
     item = Post.find(params[:id])
     render json: { post: item }
+  end
 end
